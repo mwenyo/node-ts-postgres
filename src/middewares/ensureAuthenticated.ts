fac80b3f139bebdb750d04471643272467e34580
mwenyo <mwenyo@gmail.com>
2022-01-15 19:07:02 -0300

    Mod: ensureAuthenticated => find by token

--- src/middewares/ensureAuthenticated.ts
+++ src/middewares/ensureAuthenticated.ts
@@ -1,16 +1,19 @@
 import { NextFunction, Request, Response } from 'express'
-import { decode, verify } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
+import { UserRepository } from 'src/repositories'
 
 export const ensureAuthenticated = () => {
   return async (request: Request, response: Response, next: NextFunction) => {
     try {
       const authHeader = request.headers.authorization
       const [, token] = authHeader.split(' ')
-      verify(token, process.env.ACCESS_TOKEN_SECRET)
-      const { sub } = decode(token)
-      request.userId = sub.toString()
-
-      return next()
+      const foundUser = await UserRepository().findOne({ accessToken: token })
+      if (!foundUser) return response.sendStatus(401)
+      verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err || foundUser.id.toString() !== decoded.sub) return response.sendStatus(403)
+        request.userId = decoded.sub.toString()
+        return next()
+      })
     } catch (error) {
       console.log(error)
       return response.sendStatus(401)
