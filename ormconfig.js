ec5f31db134a6a9352e44931f21cec84d121de7f
mwenyo <mwenyo@gmail.com>
2022-01-23 03:10:58 -0300

    teste4

--- ormconfig.js
+++ ormconfig.js
@@ -4,5 +4,9 @@ module.exports = {
   entities: [`${process.env.TYPEORM_ENTITIES}`],
   migrations: [`${process.env.TYPEORM_MIGRATIONS}`],
   cli: { migrationsDir: `${process.env.TYPEORM_MIGRATIONS_DIR}` },
-
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    }
+  }
 };
