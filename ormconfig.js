f90418b19084b27864942de87739d85916b75af8
mwenyo <mwenyo@gmail.com>
2022-01-23 03:08:19 -0300

    teste2

---
+++ ormconfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  type: `postgres`,
+  url: `${process.env.DATABASE_URL}`,
+  entities: [`${process.env.TYPEORM_ENTITIES}`],
+  migrations: [`${process.env.TYPEORM_MIGRATIONS}`],
+  cli: { migrationsDir: `${process.env.TYPEORM_MIGRATIONS_DIR}` },
+
+};
