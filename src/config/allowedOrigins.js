8fdffeec9c075dfff16275bb11759e1c2b9c6228
mwenyo <mwenyo@gmail.com>
2022-02-17 11:45:17 -0300

    origin

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['*', 'https://lucid-bose-92ef21.netlify.app', 'http://127.0.0.1:5500']
+const allowedOrigins = ['*', 'https://angry-davinci-fbc496.netlify.app', 'http://127.0.0.1:5500']
 
 module.exports = allowedOrigins
